75640c84b8cc70e5179f622bf8441dc92680c4ce
Alfredo Garcia <oxarbitrage@gmail.com>
2022-12-05 14:38:29 -0300

    theorem: columnround is left invariant

--- src/columnround.lean
+++ src/columnround.lean
@@ -29,4 +29,51 @@ begin
   apply rowround_is_inv,
 end
 
+/-
+  Left invariance of the columnround function: https://www.iacr.org/archive/fse2008/50860470/50860470.pdf
+
+-/
+
+--
+variables A B C D : bitvec word_len
+
+--
+def input : matrixType := (
+  (A, -B, C, -D),
+  (-A, B, -C, D),
+  (A, -B, C, -D),
+  (-A, B, -C, D)
+)
+
+--
+variable X : bitvec word_len
+
+-- TODO: move this to operations axioms and use them in `quarterround`, `rowround` and here.
+def mod_neg : Prop := ∀ X, X MOD (-X) = ZERO
+def neg_mod : Prop := ∀ X, (-X) MOD X = ZERO
+
+-- `columnround` is left invariant. 
+theorem columnround_is_left_invariant (h1 : mod_neg) (h2 : neg_mod) : 
+  utils.columnround_output (columnround (utils.columnround_input (input A B C D))) = input A B C D :=
+begin
+  unfold columnround,
+  unfold input,
+  unfold utils.columnround_input,
+  unfold utils.columnround_output,
+  unfold mod_neg at h1,
+  unfold neg_mod at h2,
+  unfold rowround,
+  unfold rowround_single,
+
+  simp only [prod.mk.inj_iff],
+
+  repeat { rw quarterround_is_left_invariant },
+
+  simp only [eq_self_iff_true, and_self],
+
+  any_goals { apply h1 },
+  any_goals { apply h2 },
+end
+
+
 end columnround
