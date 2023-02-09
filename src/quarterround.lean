66bafcbae8b035860b2a39a5ac1e72616cd07e5b
Alfredo Garcia <oxarbitrage@gmail.com>
2023-02-09 20:11:32 -0300

    fix quarterround for docs

--- src/quarterround.lean
+++ src/quarterround.lean
@@ -8,30 +8,32 @@ open utils
 namespace quarterround
 
 /-!
-  The `quarterround` function, its inverse and the invariance theorem.
+  ## Quarterround
+
+  The `quarterround` function and its properties.
 -/
 
--- `a` `b` `c` and `d` are random elements of the 4 words starting sequence
+/-- `a` `b` `c` and `d` are random elements of the 4 words starting sequence -/
 variables a b c d : bitvec word_len
 -- A new set of random elements that might or might not be the same than the ones above
 variables a' b' c' d' : bitvec word_len
 
--- Quarter round definitions
+/-! ### Definitions -/
 
 -- TODO: We are sending the 4 numbers `a` `b` `c` and `d` to each qr function but this is not
 -- always needed (example: `qr1` will just use `b` `a` and `d`)
 
--- z₁ = y₁ ⊕ ((y₀ + y₃) <<< 7)
+/-- z₁ = y₁ ⊕ ((y₀ + y₃) <<< 7) -/
 def qr1 (a b c d : bitvec word_len) := b OP (OP_RHS a d 7)
--- z₂ = y₂ ⊕ ((z₁ + y₀) <<< 9)
+/-- z₂ = y₂ ⊕ ((z₁ + y₀) <<< 9) -/
 def qr2 (a b c d : bitvec word_len) := c OP (OP_RHS (qr1 a b c d) a 9)
--- z₃ = y₃ ⊕ ((z₂ + z₁) <<< 13)
+/-- z₃ = y₃ ⊕ ((z₂ + z₁) <<< 13) -/
 def qr3 (a b c d : bitvec word_len) := d OP (OP_RHS (qr2 a b c d) (qr1 a b c d) 13)
--- z₀ = y₀ ⊕ ((z₃ + z₂) <<< 18)
+/-- z₀ = y₀ ⊕ ((z₃ + z₂) <<< 18) -/
 def qr0 (a b c d : bitvec word_len) := a OP (OP_RHS (qr3 a b c d) (qr2 a b c d) 18)
 
--- Given a sequence of 4 numbers `seq` use the four equations above to get the quarterround
--- output, which is a 4 numbers sequence too.
+/-- Given a sequence of 4 numbers `seq`, use the four equations above to get the quarterround
+output, which is a 4 numbers sequence too. -/
 @[simp] def quarterround (seq : vecType) : vecType :=
   (
     qr0 seq.fst seq.snd.fst seq.snd.snd.fst seq.snd.snd.snd,
@@ -40,18 +42,18 @@ def qr0 (a b c d : bitvec word_len) := a OP (OP_RHS (qr3 a b c d) (qr2 a b c d) 
     qr3 seq.fst seq.snd.fst seq.snd.snd.fst seq.snd.snd.snd
   )
 
--- Quarter round inverse definitions
+/-! ### Inverse definitions -/
 
--- y₀ = z₀ ⊕ ((z₃ + z₂) <<< 18)
+/-- y₀ = z₀ ⊕ ((z₃ + z₂) <<< 18) -/
 def qr0_inv (a' b' c' d' : bitvec word_len) := a' OP (operation_rhs d' c' 18)
--- y₃ = z₃ ⊕ ((z₂ + z₁) <<< 13)
+/-- y₃ = z₃ ⊕ ((z₂ + z₁) <<< 13) -/
 def qr3_inv (a' b' c' d' : bitvec word_len) := d' OP (operation_rhs c' b' 13)
--- y₂ = z₂ ⊕ ((z₁ + y₀) <<< 9)
+/-- y₂ = z₂ ⊕ ((z₁ + y₀) <<< 9) -/
 def qr2_inv (a' b' c' d' : bitvec word_len) := c' OP (operation_rhs b' (qr0_inv a' b' c' d')  9)
--- y₁ = z₁ ⊕ ((y₀ + y₃) <<< 7)
+/-- y₁ = z₁ ⊕ ((y₀ + y₃) <<< 7) -/
 def qr1_inv (a' b' c' d' : bitvec word_len) := b' OP (operation_rhs (qr0_inv a' b' c' d') (qr3_inv a' b' c' d') 7)
 
--- Puts the 4 elements that forms a quarterround inverse all together.
+/-- Puts the 4 elements that forms a quarterround inverse all together. -/
 @[simp] def quarterround_inv (seq : vecType) := (
   qr0_inv seq.fst seq.snd.fst seq.snd.snd.fst seq.snd.snd.snd,
   qr1_inv seq.fst seq.snd.fst seq.snd.snd.fst seq.snd.snd.snd,
@@ -59,7 +61,9 @@ def qr1_inv (a' b' c' d' : bitvec word_len) := b' OP (operation_rhs (qr0_inv a' 
   qr3_inv seq.fst seq.snd.fst seq.snd.snd.fst seq.snd.snd.snd
 )
 
--- The quarterround operation is fully invertible.
+/-! ### Inverse lemmas -/
+
+/-- The quarterround operation is fully invertible. -/
 @[simp] lemma quarterround_is_invertible (a b c d : bitvec word_len) : 
   qr0_inv (qr0 a b c d) (qr1 a b c d) (qr2 a b c d) (qr3 a b c d) = a ∧
   qr3_inv (qr0 a b c d) (qr1 a b c d) (qr2 a b c d) (qr3 a b c d) = d ∧ 
@@ -95,7 +99,7 @@ begin
   }
 end
 
--- Inverse of quarterround exists.
+/-- Inverse of quarterround exists. -/
 @[simp] lemma inverse_exists : ∀ (a b c d : bitvec word_len), ∃ (a' b' c' d' : bitvec word_len), 
   quarterround_inv (a', b', c', d') = (a, b, c, d) :=
 begin
@@ -110,11 +114,9 @@ begin
   simp only [xor_assoc, xor_inv, xor_zero],
 end
 
-/-
-  Inverses of the `quarterround` and `quarterround_inv` individual pieces.
--/
+/-! ### Inverses of the `quarterround` and `quarterround_inv` individual pieces lemmas -/
 
--- Inverse of `qr0` given the sequence `a, b, c, d` is `a`.
+/-- Inverse of `qr0` given the sequence `a, b, c, d` is `a`. -/
 @[simp] lemma qr0_is_inv : 
   qr0_inv (qr0 a b c d) (qr1 a b c d) (qr2 a b c d) (qr3 a b c d) = a :=
 begin
@@ -123,7 +125,7 @@ begin
     simp only [xor_assoc, xor_inv, xor_zero],
 end
 
--- Inverse of `qr1` given the sequence `a, b, c, d` is `b`.
+/-- Inverse of `qr1` given the sequence `a, b, c, d` is `b`. -/
 @[simp] lemma qr1_is_inv : 
   qr1_inv (qr0 a b c d) (qr1 a b c d) (qr2 a b c d) (qr3 a b c d) = b :=
 begin  
@@ -132,7 +134,7 @@ begin
     simp only [xor_assoc, xor_assoc, xor_assoc, xor_inv, xor_inv, xor_zero, xor_zero, xor_inv, xor_zero]
 end
 
--- Inverse of `qr2` given the sequence `a, b, c, d` is `c`.
+/-- Inverse of `qr2` given the sequence `a, b, c, d` is `c`. -/
 @[simp] lemma qr2_is_inv : 
   qr2_inv (qr0 a b c d) (qr1 a b c d) (qr2 a b c d) (qr3 a b c d) = c :=
 begin
@@ -141,7 +143,7 @@ begin
     simp only [xor_assoc, xor_assoc, xor_inv, xor_zero, xor_inv, xor_zero],
 end
 
--- Inverse of `qr3` given the sequence `a, b, c, d` is `d`.
+/-- Inverse of `qr3` given the sequence `a, b, c, d` is `d`. -/
 @[simp] lemma qr3_is_inv : 
   qr3_inv (qr0 a b c d) (qr1 a b c d) (qr2 a b c d) (qr3 a b c d) = d :=
 begin
@@ -150,7 +152,7 @@ begin
     simp only [xor_assoc, xor_inv, xor_zero],
 end
 
--- Inverse of `qr0_inv` given the sequence `a, b, c, d` is `a`.
+/-- Inverse of `qr0_inv` given the sequence `a, b, c, d` is `a`. -/
 @[simp] lemma qr0_inv_is_inv :
   qr0 (qr0_inv a b c d) (qr1_inv a b c d) (qr2_inv a b c d) (qr3_inv a b c d) = a :=
 begin
@@ -160,7 +162,7 @@ begin
     simp only [xor_assoc, xor_inv, xor_zero],
 end
 
--- Inverse of `qr1_inv` given the sequence `a, b, c, d` is `b`.
+/-- Inverse of `qr1_inv` given the sequence `a, b, c, d` is `b`. -/
 @[simp] lemma qr1_inv_is_inv :
   qr1 (qr0_inv a b c d) (qr1_inv a b c d) (qr2_inv a b c d) (qr3_inv a b c d) = b :=
 begin
@@ -169,7 +171,7 @@ begin
     simp only [xor_assoc, xor_inv, xor_zero],
 end
 
--- Inverse of `qr2_inv` given the sequence `a, b, c, d` is `c`.
+/-- Inverse of `qr2_inv` given the sequence `a, b, c, d` is `c`. -/
 @[simp] lemma qr2_inv_is_inv :
   qr2 (qr0_inv a b c d) (qr1_inv a b c d) (qr2_inv a b c d) (qr3_inv a b c d) = c :=
 begin
@@ -178,7 +180,7 @@ begin
     simp only [xor_assoc, xor_inv, xor_zero],
 end
 
--- Inverse of `qr3_inv` given the sequence `a, b, c, d` is `d`.
+/-- Inverse of `qr3_inv` given the sequence `a, b, c, d` is `d`. -/
 @[simp] lemma qr3_inv_is_inv :
   qr3 (qr0_inv a b c d) (qr1_inv a b c d) (qr2_inv a b c d) (qr3_inv a b c d) = d :=
 begin
@@ -187,29 +189,27 @@ begin
     simp only [xor_assoc, xor_inv, xor_zero],
 end
 
-/-
-  Quarterround isomorphism.
--/
+/-! ### Isomorphism -/
 
--- The identity of a `quarterround` function given a sequence is the sequence.
+/-- The identity of a `quarterround` function given a sequence is the sequence. -/
 @[simp] def id_quarterround (seq : vecType) := seq
 
--- The identity of a `quarterround_inv` function given a sequence is the sequence.
+/-- The identity of a `quarterround_inv` function given a sequence is the sequence. -/
 @[simp] def id_quarterround_inv (seq : vecType) := seq
 
--- Isomorphism condition 1 `f ∘ g = id_f`
+/-- Isomorphism condition 1 `f ∘ g = id_f` -/
 @[simp] lemma isomorphism1 (seq : vecType) : (quarterround_inv ∘ quarterround) seq = id_quarterround seq :=
 begin
   finish,
 end
 
--- Isomorphism condition 2 `g ∘ f = id_g`
+/-- Isomorphism condition 2 `g ∘ f = id_g` -/
 @[simp] lemma isomorphism2 (seq : vecType) : (quarterround ∘ quarterround_inv) seq = id_quarterround_inv seq :=
 begin
   finish,
 end
 
--- Two categories are isomrphic if `f ∘ g = id_f` and `g ∘ f = id_g`.
+/-- Two categories are isomrphic if `f ∘ g = id_f` and `g ∘ f = id_g`. -/
 @[simp] theorem quarterround_is_isomorphic (seq : vecType) :
   (quarterround_inv ∘ quarterround) seq = id_quarterround seq ∧
   (quarterround ∘ quarterround_inv) seq = id_quarterround_inv seq :=
@@ -217,19 +217,22 @@ begin
   simp only [isomorphism1, eq_self_iff_true, isomorphism2, and_self],
 end
 
-/-
-  Left invariance of the quarterround function: https://www.iacr.org/archive/fse2008/50860470/50860470.pdf
+/-!
+  ### Invariance
 
-  We prove each single `qr{0..3}` is invariant to the left and derive that the full quarterround
-  function is left invariant.
+  We prove each single `qr{0..3}` is invariant to the left
+  when fed with a crafted sequence and by this derive that the
+  full quarterround function is left invariant.
 
   Theorem 1 of the paper:
 
   > For any 32-bit value A, an input of the form (A −A A −A) is left invariant by the
   > quarterround function, where −A represents the only 32-bit integer satisfying A + (−A) = 0 (mod 232).
+
+  https://www.iacr.org/archive/fse2008/50860470/50860470.pdf
 -/
 
--- 
+/-- `qr1` of `a, -a, a, -a` is `-a`. -/
 @[simp] lemma qr1_is_left_invariant : qr1 a (-a) a (-a) = -a := 
 begin
   unfold qr1,
@@ -238,7 +241,7 @@ begin
   simp only [mod_neg, zero_rotl, xor_zero],
 end
 
---
+/-- `qr1` of `-a, a, -a, a` is `a`. -/
 @[simp] lemma qr1_is_left_invariant' : qr1 (-a) a (-a) a = a := 
 begin
   unfold qr1,
@@ -247,7 +250,7 @@ begin
   simp only [neg_mod, zero_rotl, xor_zero],
 end
 
---
+/-- `qr2` of `a, -a, a, -a` is `a`. -/
 @[simp] lemma qr2_is_left_invariant : qr2 a (-a) a (-a) = a := 
 begin
   unfold qr2,
@@ -257,7 +260,7 @@ begin
   simp only [neg_mod, zero_rotl, xor_zero],
 end
 
---
+/-- `qr2` of `-a, a, -a, a` is `-a`. -/
 @[simp] lemma qr2_is_left_invariant' : qr2 (-a) a (-a) a = -a := 
 begin
   unfold qr2,
@@ -267,7 +270,7 @@ begin
   simp only [mod_neg, zero_rotl, xor_zero],
 end
 
---
+/-- `qr3` of `a, -a, a, -a` is `-a`. -/
 @[simp] lemma qr3_is_left_invariant : qr3 a (-a) a (-a) = -a := 
 begin
   unfold qr3,
@@ -279,7 +282,7 @@ begin
   simp only [mod_neg, zero_rotl, xor_zero],
 end
 
---
+/-- `qr3` of `-a, a, -a, a` is `a`. -/
 @[simp] lemma qr3_is_left_invariant' : qr3 (-a) a (-a) a = a := 
 begin
   unfold qr3,
@@ -291,7 +294,7 @@ begin
   simp only [neg_mod, zero_rotl, xor_zero],
 end
 
---
+/-- `qr0` of `a, -a, a, -a` is `a`. -/
 @[simp] lemma qr0_is_left_invariant : qr0 a (-a) a (-a) = a := 
 begin
   unfold qr0,
@@ -303,7 +306,7 @@ begin
   simp only [neg_mod, zero_rotl, xor_zero],
 end
 
---
+/-- `qr0` of `-a, a, -a, a` is `-a`. -/
 @[simp] lemma qr0_is_left_invariant' : qr0 (-a) a (-a) a = -a := 
 begin
   unfold qr0,
@@ -315,62 +318,85 @@ begin
   simp only [mod_neg, zero_rotl, xor_zero],
 end
 
-
---
+/-- The full `quarterround` function produces `a, -a, a, -a` when fed with `a, -a, a, -a` -/
 @[simp] theorem quarterround_is_left_invariant : quarterround (a, -a, a, -a) = (a, -a, a, -a) :=
 begin
   simp only [quarterround, qr0_is_left_invariant, qr1_is_left_invariant, qr2_is_left_invariant, qr3_is_left_invariant],
 end
 
---
+/-- The full `quarterround` function produces `-a, a, -a, a` when fed with `-a, a, -a, a` -/
 @[simp] theorem quarterround_is_left_invariant' : quarterround (-a, a, -a, a) = (-a, a, -a, a) :=
 begin
   simp only [quarterround, qr0_is_left_invariant', qr1_is_left_invariant', qr2_is_left_invariant', qr3_is_left_invariant'],
 end
 
-/-
+/-!
+  ### Known variance of `quarterround` if diff of each input is 2³¹
+
   `quarterround` function will only flip the most significant bit when two set of elements is
-  provided where the difference (or xor) between each element is 2^31.
+  provided where the difference (or xor) between each element is 2³¹.
 -/
 
 -- Have 4 random vectors to be used as quarterround inputs.
 variables m n o p : bitvec 32
 
--- define that x xor 2^31 = flip msb bit only, leave the rest as is.
--- TODO: this could be proved.
+/--
+Define that x xor 2³¹ = flip msb bit only, leave the rest as is.
+
+#### TODO:
+
+This could be proved.
+-/
 def craft (m : bitvec word_len) : bitvec word_len :=
   if m.head = ff then (bitvec.one 1).append m.tail else (bitvec.zero 1).append m.tail
 
--- a shortcut to a vector head.
+/-- a shortcut to a vector head. -/
 def msb (m : bitvec word_len) : bool := m.head
--- a shortcut for a vector tail.
+/-- a shortcut for a vector tail. -/
 def rest (m : bitvec word_len) : bitvec (word_len - 1) := m.tail
 
--- lets suppose the msb of any uncrafted bitvec that we will send to quarterround is always `ff`.
--- TODO: this does not need to be the case, everything should work in a very similar way if the head is 1
--- as it will gets flipped to 0 but we do this for simplicity by now.
--- Basically by restricting the msb to be 0 we are saying that the number is smaller than 2^31.
+/--
+Lets suppose the msb of any uncrafted bitvec that we will send to quarterround is always `ff`.
+
+#### TODO:
+
+This does not need to be the case, everything should work in a very similar way if the head is 1
+as it will gets flipped to 0 but we do this for simplicity by now.
+Basically by restricting the msb to be 0 we are saying that the number is smaller than 2^31. -/
 constant h_msb : m.head = ff
 
--- define a type notation which is an abstract function that can represent qr0 qr1 qr2 qr3
+-- Define a type notation which is an abstract function that can represent `qr0` `qr1` `qr2` `qr3`
 local notation `qrX` := bitvec word_len → bitvec word_len → bitvec word_len → bitvec word_len → bitvec word_len
 
--- assumes that any qrX function that is feeded with crafted numbers will result in a head of 1
--- and the rest or tail equal to the tail of the uncrafted number.
--- TODO: this needs to be proved.
+/--
+Assumes that any qrX function that is feeded with crafted numbers will result in a head of 1
+and the rest or tail equal to the tail of the uncrafted number.
+
+#### TODO:
+
+This needs to be proved.
+-/
 constant qrX_crafted (f : qrX) :
   f (craft m) (craft n) (craft o) (craft p) = (bitvec.one 1).append (f m n o p).tail
 
--- a random bitvector of size `word_len - 1` representing the tail of a bitvector of size `word_len`.
+/--
+A random bitvector of size `word_len - 1` representing the tail of a bitvector of size `word_len`.
+-/
 variable tail_placeholder : bitvec (word_len - 1)
--- the head (msb) of anything that starts with a 1 and then stuff is appended should be always 1.
--- TODO: should be easy to prove ?
+/--
+The head (msb) of anything that starts with a 1 and then stuff is appended should be always 1.
+#### TODO:
+
+Should be easy to prove ? -/
 constant msb_of_one_append : vector.head ((bitvec.one 1).append tail_placeholder) = tt
--- the rest of the bitvector where 1 gets appended with the tail of the bitvector is the tail of the bitvector.
--- TODO: should be easy to prove ?
+/-- The rest of the bitvector where 1 gets appended with the tail of the bitvector is the tail of the bitvector.
+#### TODO:
+
+should be easy to prove ?
+-/
 constant rest_of_one_append : vector.tail m = ((bitvec.one 1).append (m).tail).tail
 
--- for any individual qrX function, when feeded with crafted data the difference is carried.
+/-- For any individual qrX function, when feeded with crafted data the difference is carried. -/
 lemma qrX_difference_is_carried (f : qrX) :
   msb (f m n o p) ≠ msb (f (craft m) (craft n) (craft o) (craft p)) ∧
     rest (f m n o p) = rest (f (craft m) (craft n) (craft o) (craft p)) :=
@@ -389,31 +415,31 @@ begin
   }
 end
 
--- qr1 difference is carried
+/-- `qr1` difference is carried when fed with crafted data. -/
 lemma qr1_difference_is_carried :
   msb (qr1 m n o p) ≠ msb (qr1 (craft m) (craft n) (craft o) (craft p)) ∧
     rest (qr1 m n o p) = rest (qr1 (craft m) (craft n) (craft o) (craft p)) :=
 by apply qrX_difference_is_carried m n o p qr1
 
--- qr2 difference is carried
+/-- `qr2` difference is carried when fed with crafted data. -/
 lemma qr2_difference_is_carried :
   msb (qr2 m n o p) ≠ msb (qr2 (craft m) (craft n) (craft o) (craft p)) ∧
     rest (qr2 m n o p) = rest (qr2 (craft m) (craft n) (craft o) (craft p)) :=
 by apply qrX_difference_is_carried m n o p qr2
 
--- qr3 difference is carried
+/-- `qr3` difference is carried when fed with crafted data. -/
 lemma qr3_difference_is_carried :
   msb (qr3 m n o p) ≠ msb (qr3 (craft m) (craft n) (craft o) (craft p)) ∧
     rest (qr3 m n o p) = rest (qr3 (craft m) (craft n) (craft o) (craft p)) :=
 by apply qrX_difference_is_carried m n o p qr3
 
--- qr0 difference is carried
+/-- `qr0` difference is carried when fed with crafted data. -/
 lemma qr0_difference_is_carried :
   msb (qr0 m n o p) ≠ msb (qr0 (craft m) (craft n) (craft o) (craft p)) ∧
     rest (qr0 m n o p) = rest (qr0 (craft m) (craft n) (craft o) (craft p)) :=
 by apply qrX_difference_is_carried m n o p qr0
 
--- full quarterround carries the difference when feeded with crafted data.
+/-- Full `quarterround` carries the difference when fed with crafted data. -/
 lemma quarterround_difference_is_carried :
   -- z0 only changes in the msb
   (msb (quarterround (m, n, o, p)).fst ≠ msb (quarterround ((craft m), (craft n), (craft o), (craft p))).fst ∧
