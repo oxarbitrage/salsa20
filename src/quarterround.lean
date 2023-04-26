8b8a99a9ec714ed8a7fd400b328baa6cf7cf94bf
Alfredo Garcia <oxarbitrage@gmail.com>
2023-04-26 10:11:50 -0300

    simplify quarterround a bit

--- src/quarterround.lean
+++ src/quarterround.lean
@@ -26,90 +26,53 @@ diagram.
 /-- There is a functor between `vecType` and `wordType`. -/
 variables (F : vecType ⥤ wordType)
 
-/-- Return `(x0, 1, 1, 1)` given an input vector `(x0, x1, x2, x3)`. -/
-@[simp] def first_f : vecType → vecType
-| input := (input.fst, 1, 1, 1)
+/-- Return `(xᵢ, 1, 1, 1)` given an input vector `(x₀, x₁, x₂, x₃)` and a position `i`. -/
+def value_at_position (input : vecType) (i : fin 3) : vecType :=
+match i.val with
+| 0 := (input.fst, 1, 1, 1)
+| 1 := (input.snd.fst, 1, 1, 1)
+| 2 := (input.snd.snd.fst, 1, 1, 1)
+| 3 := (input.snd.snd.snd, 1, 1, 1)
+| _ := (1, 1, 1, 1)
+end
 
-/-- Get the first object of the `vecType` product. -/
-@[simp] def first (input : vecType) := F.obj (first_f input)
- 
-/-- Given `(input, 1, 1, 1)` it is safe for us to ignore all the ones and return just `input` as this is a product. -/
-constant shrink (input: wordType) : F.obj (input, 1, 1, 1) = input
+/-- Make `value_at_position` the functor morphism. -/
+def value_at_functor (input : vecType) (pos : fin 3) := F.obj (value_at_position input pos)
 
-/-- -/
-lemma just_first (input: wordType) : first F (input, 1, 1, 1) = input :=
+/-- Given `(a, o, p, q)` it is safe for us to ignore `o`, `p`, `q` and return just `a`
+if `value_at_position` was used, `o`, `p` and `q` will be all `1` (product identity). -/
+constant shrink (a o p q: wordType) (pos : fin 3) : F.obj (value_at_position (a, o, p, q) pos) = a
+
+/-- The identity in the `wordType` category. -/
+def id_wordType := 𝟙 wordType
+
+/-- Make sure `value_at_functor` just return the identity of the first number of the product in the `wordType` category. -/
+lemma just_first (a : wordType) (pos : fin 3) : value_at_functor F (a, 1, 1, 1) pos = id_wordType a :=
 begin
+  rw [value_at_functor, id_wordType],
   norm_num,
   rw shrink,
 end
-
-@[simp] def id_wordType := 𝟙 wordType
-
-/-- Given `a`, `b`, `c` and `d` objects of the `wordType` category:
-- Form `(a, b, c, d)` in the `vecType` category. 
-- The first element of `(a, b, c, d)` in the `vecType` category correspond to `a` in the `wordType` one.  -/
-lemma first_shrink (a b c d : wordType) : (first F) (a, b, c, d) = id_wordType a :=
-begin
-  simp only [first, id_wordType, first_f, types_id_apply],
-  apply just_first,
-end
-
-/-- Return `(x1, 1, 1, 1)` given an input vector `(x0, x1, x2, x3)`. -/
-@[simp] def second_f : vecType → vecType
-| input := (input.snd.fst, 1, 1, 1)
-
-/-- Get the second object of the `vecType` product. -/
-@[simp] def second (input : vecType) := F.obj (second_f input)
-
-/-- -/
-lemma just_second (input: wordType) : second F (1, input, 1, 1) = input :=
-begin
-  norm_num,
-  rw shrink,
-end
-
-/-- Given `a`, `b`, `c` and `d` objects of the `wordType` category:
-- Form `(a, b, c, d)` in the `vecType` category. 
-- The second element of `(a, b, c, d)` in the `vecType` category correspond to `b` in the `wordType` one.  -/
-lemma second_shrink (a b c d : wordType) : (second F) (a, b, c, d) = id_wordType b :=
-begin
-simp only [second, id_wordType, second_f, types_id_apply],
-  apply just_second,
-end
-
-/-- Return `(x2, 0, 0, 0)` given an input vector `(x0, x1, x2, x3)`. -/
-def third_f : vecType → vecType
-| input := (input.snd.snd.fst, 0, 0, 0)
-
-/-- Get the third object of the `vecType` product. -/
-def third (input : vecType) := F.obj (third_f input)
-
-/-- Return `(x3, 0, 0, 0)` given an input vector `(x0, x1, x2, x3)`. -/
-def fourth_f : vecType → vecType
-| input := (input.snd.snd.snd, 0, 0, 0)
-
-/-- Get the fourth object of the `vecType` product. -/
-def fourth (input : vecType) := F.obj (third_f input)
 
 /-- Return `(y0, y3)` given an input vector `(y0, y1, y2, y3)` and a `wordType` that in this case 
 will be ignored but it is here to be compatible with other buildmod functions. -/
 def buildmod1 : vecType → wordType → wordType × wordType
-| input _ := (first F input, fourth F input)
+| input _ := (value_at_functor F input 0, value_at_functor F input 3)
 
 /-- Return `(y1, rotlres)` given an input vector `(y0, y1, y2, y3)` and `rotlres`. -/
 def buildxor1 : vecType → wordType → wordType × wordType
-| input b := (second F input, b)
+| input b := (value_at_functor F input 1, b)
 
 /-- z₁ = y₁ ⊕ ((y₀ + y₃) <<< 7) -/
 def z1 (input : vecType) := ↾ buildmod1 F input ≫ mod ≫ rotl7 ≫ buildxor1 F input ≫ xor
 
 /-- Return `(z1, y0)` given an input vector `(y0, y1, y2, y3)` and `z1`. -/
 def buildmod2 : vecType → wordType → wordType × wordType
-| input z1 := (z1, first F input)
+| input z1 := (z1, value_at_functor F input 0)
 
 /-- Return `(y2, rotlres)` given an input vector `(y0, y1, y2, y3)` and `rotlres`. -/
 def buildxor2 : vecType → wordType → wordType × wordType
-| input b := (third F input, b)
+| input b := (value_at_functor F input 2, b)
 
 /-- z₂ = y₂ ⊕ ((z₁ + y₀) <<< 9) -/
 def z2 (input : vecType) := ↾ buildmod2 F input ≫ mod ≫ rotl9 ≫ buildxor2 F input ≫ xor
@@ -120,7 +83,7 @@ def buildmod3 : vecType → wordType → wordType → wordType × wordType
 
 /-- Return `(y3, rotlres)` given an input vector `(y0, y1, y2, y3)` and `rotlres`. -/
 def buildxor3 : vecType → wordType → wordType × wordType
-| input b := (fourth F input, b)
+| input b := (value_at_functor F input 3, b)
 
 /-- z₃ = y₃ ⊕ ((z₂ + z₁) <<< 13) -/
 def z3 (input : vecType) :=
@@ -132,7 +95,7 @@ def buildmod0 : vecType → wordType → wordType → wordType × wordType
 
 /-- Return `(y0, rotlres)` given an input vector `(y0, y1, y2, y3)` and `rotlres`. -/
 def buildxor0 : vecType → wordType → wordType × wordType
-| input b := (first F input, b)
+| input b := (value_at_functor F input 0, b)
 
 /-- z₀ = y₀ ⊕ ((z₃ + z₂) <<< 18) -/
 def z0 (input : vecType) := 
