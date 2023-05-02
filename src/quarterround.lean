ae4729ed0ca90a14900db5f6486b0576fab48d25
Alfredo Garcia <oxarbitrage@gmail.com>
2023-05-02 17:16:46 -0300

    write quarterround in terms of isomorphisms

--- src/quarterround.lean
+++ src/quarterround.lean
@@ -24,85 +24,85 @@ variable y₀y₁y₂y₃ : Type u
 constant m₁ : Type u
 
 /-- The `mod1` relation given `y₀` and `y₃` objects. -/
-variable mod1 : (y₀y₁y₂y₃ ⟶ y₀) × (y₀y₁y₂y₃ ⟶ y₃) ⟶ m₁
+def mod1 (I0 : y₀y₁y₂y₃ ≅ y₀) (I3 : y₀y₁y₂y₃ ≅ y₃) := m₁
 
 /-- The response of a `rotl7` operation as an object of the `Type u` category. -/
 variable r₁ : Type u
 
 /-- The `rotl7` operation will get the result of `mod1` as an input and return an `r₁` object of 
 the `Type u` category. -/
-variable rotl7 : m₁ ⟶ r₁
+noncomputable def rotl7 (I : m₁ ≅ r₁) := I.hom
 
 /-- The result of `xor1` operation betwen `y₁` and `r₁` is an object of the `Type u` category. -/
 constant z₁ : Type u
 
 /-- The `xor1` operation return `z₁`. -/
-variable xor1 : (y₀y₁y₂y₃ ⟶ y₁) × r₁ ⟶ z₁
+def xor1 (I0 : y₀y₁y₂y₃ ≅ y₁) := z₁
 
 /-- The response of the `mod2` operation as an object of the `Type u` category. -/
 constant m₂ : Type u
 
 /-- The `mod2` relation given `z₁` and `y₀` objects. -/
-variable mod2 : z₁ × (y₀y₁y₂y₃ ⟶ y₀) ⟶ m₂
+def mod2 (I : y₀y₁y₂y₃ ≅ y₀) := m₂
 
 /-- The response of a `rotl9` operation as an object of the `Type u` category. -/
 variable r₂ : Type u
 
 /-- The `rotl9` operation will get the result of `mod2` as an input and return an `r₂` object of 
 the `Type u` category. -/
-variable rotl9 : m₂ ⟶ r₂
+noncomputable def rotl9 (I : m₂ ≅ r₂) := I.hom
 
 /-- The result of `xor2` operation betwen `y₁` and `r₂` is an object of the `Type u` category. -/
 constant z₂ : Type u
 
 /-- The `xor2` operation return `z₂`. -/
-constant xor2 : (y₀y₁y₂y₃ ⟶ y₂) × r₂ ⟶ z₂
+def xor2 (I : y₀y₁y₂y₃ ≅ y₂) := z₂
 
 /-- The response of the `mod3` operation as an object of the `Type u` category. -/
 constant m₃ : Type u
 
 /-- The `mod3` relation given `z₂` and `z₁` objects. -/
-variable mod3 : z₂ × z₁ ⟶ m₃
+noncomputable def mod3 := m₃
 
 /-- The response of a `rotl13` operation as an object of the `Type u` category. -/
 variable r₃ : Type u
 
 /-- The `rotl13` operation will get the result of `mod3` as an input and return an `r₃` object of 
 the `Type u` category. -/
-variable rotl13 : m₃ ⟶ r₃
+noncomputable def rotl13 (I : m₃ ≅ r₃) := I.hom
 
 /-- The result of `xor3` operation betwen `y₃` and `r₃` is an object of the `Type u` category. -/
 variable z₃ : Type u
 
 /-- The `xor3` operation return `z₃`. -/
-variable xor3 : (y₀y₁y₂y₃ ⟶ y₃) × r₃ ⟶ z₃
+def xor3 (I : y₀y₁y₂y₃ ≅ y₃) := z₃
 
 /-- The response of the `mod0` operation as an object of the `Type u` category. -/
 constant m₀ : Type u
 
 /-- The `mod0` relation given `z₃` and `z₂` objects. -/
-variable mod0 : z₃ × z₂ ⟶ m₀
+noncomputable def mod0 := m₀
 
 /-- The response of a `rotl18` operation as an object of the `Type u` category. -/
 variable r₀ : Type u
 
 /-- The `rotl18` operation will get the result of `mod0` as an input and return an `r₀` object of 
 the `Type u` category. -/
-variable rotl18 : m₀ ⟶ r₀
+noncomputable def rotl18 (I : m₀ ≅ r₀) := I.hom
 
 /-- The result of `xor0` operation betwen `y₀` and `r₀` is an object of the `Type u` category. -/
 variable z₀ : Type u
 
 /-- The `xor0` operation return `z₀`. -/
-variable xor0 : (y₀y₁y₂y₃ ⟶ y₀) × r₀ ⟶ z₀
+def xor0 (I : y₀y₁y₂y₃ ⟶ y₀) := z₀ 
 
 /-- The result of a full `quarterround` operation. -/
 variable z₀z₁z₂z₃ : Type u
 
 /-- Convert an object of `Type u` into another object of `Type u`. -/
-constant quarterround : y₀y₁y₂y₃ ⟶ z₀z₁z₂z₃
+def quarterround (I : y₀y₁y₂y₃ ≅ z₀z₁z₂z₃) := I.hom
 
-/-- Quarterround is isomorphic. -/
-constant quarterround_iso : y₀y₁y₂y₃ ≅ z₀z₁z₂z₃
+/-- Quarterround inverse. -/
+def quarterround_inv (I : y₀y₁y₂y₃ ≅ z₀z₁z₂z₃) := I.inv
 
 end quarterround
